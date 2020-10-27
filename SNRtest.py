bdcc534a7e25e49aecbd1872a1824e2c70e4e836
Alex <alex@Amrits-MacBook-Pro-2.local>
2020-10-27 13:46:31 -0700

    added dummy target

--- SNRtest.py
+++ SNRtest.py
@@ -13,18 +13,19 @@ from numpy import sin, pi
 
 # Create a window to draw in
 win = visual.Window((600, 600), allowGUI=False, monitor='testMonitor', units='deg',blendMode='avg',allowStencil=True)
+nTrials = 10
 
 # Initialize some stimuli
 # create the mask we will draw the stimulus through - can only use one so draw one annulus and we will rotate it
-mask = visual.RadialStim(win, tex='none', color=1, size=10,
-    visibleWedge=[10, 170], radialCycles=4, angularCycles=8, interpolate=False,
+mask = visual.RadialStim(win, tex='none', color=1, size=1,
+    visibleWedge=[2, 178], radialCycles=4, angularCycles=8, interpolate=False,
     autoLog=False)  # this stim changes too much for autologging to be useful
 # gratings for plaid stimulus
 grating1 = visual.GratingStim(
-    win, tex="sin", mask="circle", texRes=128,
+    win, tex="sin", mask="circle", texRes=512,
     color='white', size=10, sf=2, ori=45, depth=0.5, opacity=0.5, autoLog=False)
 grating2 = visual.GratingStim(
-    win, tex="sin", mask="circle", texRes=128,
+    win, tex="sin", mask="circle", texRes=512,
     color='white', size=10, sf=2, ori=-45, depth=0.5, opacity=0.5, autoLog=False)
 # 0 contrats grating + gauss mask to act as target(s)
 grating3 =visual.GratingStim(
@@ -36,7 +37,7 @@ central_grey = visual.PatchStim(win, tex=None, mask='raisedCos',
                              color=0,size=1)
 
 message = visual.TextStim(
-    win, text='Any key to quit',
+    win, text='Press q to quit',
     pos=(-0.95, -0.95), units='norm',
     anchorVert='bottom', anchorHoriz='left')
 
@@ -44,38 +45,58 @@ trialClock = core.Clock()
 t = 0
 
 # create aperture from mask
-aperture = visual.Aperture(win, size=0.9, shape=mask.verticesPix)  # try shape='square'
+aperture = visual.Aperture(win, size=.5, shape=mask.verticesPix)  # try shape='square'
 
-while not event.getKeys() and t < 20:  # quits after 20 secs
-    t = trialClock.getTime()
-    # set grating contrast
-    grating1.contrast = sin(t * pi * 4)
-    grating2.contrast = sin(t * pi * 4)
-    
-    # initally draw right side
-    aperture.ori=0
-    aperture.enabled = True  # enabled by default when created
-    # draw plaid
-    grating1.draw()
-    grating2.draw()
-    # draw target
-    grating3.draw()
-    
-    # then rorate annulus to draw left
-    aperture.ori=180    
-    # draw plaid
-    grating1.draw()
-    grating2.draw()
-    
-    # release aperture
-    aperture.enabled = False  # enabled by default when created
-    
-    # draw fixation area
-    central_grey.draw()
-    
-    message.draw()
+for x in range(nTrials):
+    trialClock.reset()
+    t = 0
+    while  t < 10:  # quits after 20 secs
+        t = trialClock.getTime()
+        # set grating contrast
+        grating1.contrast = sin(t * pi * 4)
+        grating2.contrast = sin(t * pi * 4)
+        
+        # initally draw right side
+        aperture.ori=0
+        aperture.enabled = True  # enabled by default when created
+        # draw plaid
+        grating1.draw()
+        grating2.draw()
+        # draw target
+        if t<5 and t>4:
+            grating3.draw()
+        
+        # then rorate annulus to draw left
+        aperture.ori=180    
+        # draw plaid
+        grating1.draw()
+        grating2.draw()
+        
+        # release aperture
+        aperture.enabled = False  # enabled by default when created
+        
+        # draw fixation area
+        central_grey.draw()
+        
+        message.draw()
 
-    win.flip()
+        win.flip()
+        for key in event.getKeys():
+            keyTime=trialClock.getTime()
+            if key in ['escape','q']:
+                win.close()
+                core.quit()
+    trialClock.reset()
+    t=0
+    while t < 10:
+        t = trialClock.getTime()
+        message.draw()
+        win.flip()
+        for key in event.getKeys():
+            keyTime=trialClock.getTime()
+            if key in ['escape','q']:
+                win.close()
+                core.quit()
 
 win.close()
 core.quit()
