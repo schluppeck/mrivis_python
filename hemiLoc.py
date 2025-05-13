d512ff09ba1e53484a2037b5aa82c42c63c20165
Denis Schluppeck <denis.schluppeck@gmail.com>
2025-05-13 13:45:13 +0100

    minimal working version of hemiLoc.py - some refactoring

--- hemiLoc.py
+++ hemiLoc.py
@@ -1,231 +1,214 @@
 #!/usr/bin/env python
-# hemiLoc v2.0
+# hemiLoc v2.1
 
 # stimuli for visual hemifield localisation
+
+# ./hemiLoc.py -h # for help
+# ./hemiLoc.py -g # for entering values via GUI
+
 # input arguments:
 # 1 - blockLength - How long each eye is stimulated for
 # 2 - numBlocks - How many blocks of hemifield stimulation to run for (must be even for equal number of R and L)
 # 3 - nullPeriod - how long the blank period at the beginning of the session should run for
 # 4 - stimSize - size of the stimulus in proportion to screen height
 
-# parameters can be set using the input arguments above - if the stimulus goes off the edge of the screen, reduce stimSize, or increase if there's dead-space at the edge of the screen.'
-# stimulus length = (blockLength*numBlocks)+nullPeriod
-
-# parameters can be set either via commnand line arguments or GUI
-# if all arguments passed in, assume user is happy with parameters and GUI will not appear
-
-from psychopy import core, visual, event, gui
+import psychopy
+from psychopy import core, visual, event, gui, plugins
 from numpy import sin, pi
-import math,sys,time
+import math
+import sys
+import time
 import numpy as np
 
-if len(sys.argv)>1:
-    blockLength=int(sys.argv[1])
-else:
-    blockLength=16
+# provide a compatibility layer for newer versions of PsychoPy
+# and some site-specific parameters
+import compatibility
+from compatibility import waitForScanner
 
-if len(sys.argv)>2:
-    numBlocks=int(sys.argv[2])
-else:
-    numBlocks=10
+parser = compatibility.setupParser()
+parser.add_argument('-bl', '--blockLength', default=12, type=float,
+                    help='How long is the block on? (seconds)')
+parser.add_argument('-nb', '--numBlocks', default=10, type=int,
+                    help='How many blocks?')
+parser.add_argument('-np', '--nullPeriod', default=10,  type=float,
+                    help='Duration of gray screen at start (seconds)')
+parser.add_argument('-ss', '--stimSize', default=1.0, type=float,
+                    help='Stimulus size (fraction of screen height)')
+parser.add_argument('-fp', '--flashPeriod', default=0.25, type=float,
+                    help='Flash period (seconds)')
+parser.add_argument('-g', help='Use the GUI to set params',
+                    dest='useGUI', action='store_true')
+parser.add_argument('-v', help='Set verbose output',
+                    dest='verbose', action='store_true')
 
-if len(sys.argv)>3:
-    nullPeriod=int(sys.argv[3])
-else:
-    nullPeriod=blockLength/2
+# specific help for this program
+parser.description = '''
+Visual stimulus that alternates a hemifield stimulus between left and right
+'''
 
-if len(sys.argv)>4:
-    stimSize=float(sys.argv[4])
-else:
-    stimSize=1
-    
-if len(sys.argv)>5:
-    flashPeriod=float(sys.argv[5])
-else:
-    flashPeriod=0.25
+parser.epilog = './hemiLoc.py -bl 12 -nb 10 -np 0 -ss 1.0 -fp 0.25\n'
+args = parser.parse_args()
 
-params = {
-        'blockLength':blockLength,
-        'numBlocks': numBlocks,
-        'nullPeriod': nullPeriod,
-        'stimSize': stimSize,
-        'flashPeriod': flashPeriod,
-        }
+# create a dictionary of parameters that can be passed to the GUI function
+params = args.__dict__.copy()
+
 tip = {
-        'blockLength':'length of on/off cycle',
-        'numBlocks': 'number of blocks to run for',
-        'nullPeriod': 'initial rest period',
-        'stimSize': 'size of the stimulus in proportion to screen height',
-        'flashPeriod': 'flash period (on/off cycle) in s',
-        }
-params['timeStr']= time.strftime("%b_%d_%H%M", time.localtime())
+    'blockLength': 'length of on/off cycle',
+    'numBlocks': 'number of blocks to run for',
+    'nullPeriod': 'initial rest period',
+    'stimSize': 'size of the stimulus in proportion to screen height',
+    'flashPeriod': 'flash period (on/off cycle) in s',
+}
+params['timeStr'] = compatibility.getTimeStr()
 
-if len(sys.argv)<5:
+if params['useGUI']:
+    # if the user wants to use the GUI, create a dialog box
     dlg = gui.DlgFromDict(
-            dictionary=params,
-            title="Hemifield Localizer",
-            fixed=['timeStr'],
-            sort_keys=True,
-            tip=tip)
+        dictionary=params,
+        title="Hemifield Localizer",
+        fixed=['timeStr'],
+        sortKeys=True,
+        tip=tip)
 
-    if dlg.OK:
-        print(params)
-    else:
-        core.quit() #user cancelled. quit
-else:
-    print(params)
-    
+    if not (dlg.OK):
+        core.quit()  # user cancelled. quit
+
 blockLength = params['blockLength']
 numBlocks = params['numBlocks']
 nullPeriod = params['nullPeriod']
 stimSize = params['stimSize']
+flashPeriod = params['flashPeriod']
 
-#create a window to draw in
-myWin =visual.Window((1280,800),allowGUI=False,
-bitsMode=None, units='height', fullscr=0,winType='pyglet',monitor='testMonitor', color=0)
+# create a window to draw in
+myWin = visual.Window(compatibility.SCREEN_SIZE,
+                      allowGUI=False,
+                      bitsMode=None,
+                      units='height',
+                      fullscr=0,
+                      winType='pyglet',
+                      monitor='testMonitor',
+                      checkTiming=compatibility.CHECK_TIMING,
+                      color=0)
 
-fixLength=1.0/2
-my_colors = {'red':[1,0,0],
-             'green':[0,1,0],
-             'blue':[0,0,1],
-             'yellow':[1,1,0]}
+fixLength = 1.0/2
+my_colors = {'red': [1, 0, 0],
+             'green': [0, 1, 0],
+             'blue': [0, 0, 1],
+             'yellow': [1, 1, 0]}
 
-rgb = np.array([1.,1.,1.])
+rgb = np.array([1., 1., 1.])
 two_pi = 2*np.pi
 
-rotationRate = (1.0/blockLength) #revs per sec
+rotationRate = (1.0/blockLength)  # revs per sec
 
-#flashPeriod = 0.125 
+fixation = visual.ShapeStim(myWin,
+                            lineColor='white',
+                            lineWidth=2.0,
+                            vertices=((-0.5, 0), (0.5, 0), (0, 0),
+                                      (0, 0.5), (0, -0.5)),
+                            interpolate=False,
+                            closeShape=False,
+                            pos=(0, 0))
 
-fixation = visual.ShapeStim(myWin, 
-            lineColor='white', 
-            lineWidth=2.0, 
-            vertices=((-0.5, 0), (0.5, 0), (0,0), (0,0.5), (0,-0.5)), 
-            interpolate=False, 
-            closeShape=False, 
-            pos=(0,0)) 
+fixation = visual.PatchStim(myWin, tex=None, mask='circle', sf=0, size=.2,
+                            name='fixation', autoLog=False, color=(-1, -1, -1), pos=(0, 0))
 
-fixation = visual.PatchStim(myWin, tex=None, mask='circle',sf=0, size=.2,
-                            name='fixation', autoLog=False,color=(-1,-1,-1),pos=(0,0))
+central_grey = visual.PatchStim(myWin, tex=None, mask='circle',
+                                color=0*rgb,
+                                size=.2*3)
 
-central_grey = visual.PatchStim(myWin, tex=None, mask='circle', 
-                                                    color=0*rgb, 
-                                                    size=.2*3)
+fixation = visual.PatchStim(myWin, tex=None, mask='circle', color=1*rgb,
+                            size=1, units='deg')
 
-fixation = visual.PatchStim(myWin, tex=None, mask = 'circle',color=1*rgb,
-                                size=1,units='deg')
-
-wedge1 = visual.RadialStim(myWin, tex='sqrXsqr', color=1,size=stimSize,
+wedge1 = visual.RadialStim(myWin, tex='sqrXsqr', color=1, size=stimSize,
                            visibleWedge=[0, 181], radialCycles=4, angularCycles=8, interpolate=False,
-                           autoLog=False,ori=0,pos=(0,0))#this stim changes too much for autologging to be useful
-wedge2 = visual.RadialStim(myWin, tex='sqrXsqr', color=-1,size=stimSize,
+                           autoLog=False, ori=0, pos=(0, 0))  # this stim changes too much for autologging to be useful
+wedge2 = visual.RadialStim(myWin, tex='sqrXsqr', color=-1, size=stimSize,
                            visibleWedge=[0, 181], radialCycles=4, angularCycles=8, interpolate=False,
-                           autoLog=False,ori=0,pos=(0,0))#this stim changes too much for autologging to be useful
+                           autoLog=False, ori=0, pos=(0, 0))  # this stim changes too much for autologging to be useful
 
-wedge3 = visual.RadialStim(myWin, tex='sqrXsqr', color=1,size=stimSize,
-                           visibleWedge=[0,181], radialCycles=4, angularCycles=8, interpolate=False,
-                           autoLog=False,ori=180,pos=(0,0))#this stim changes too much for autologging to be useful
-wedge4 = visual.RadialStim(myWin, tex='sqrXsqr', color=-1,size=stimSize,
-                           visibleWedge=[0,181], radialCycles=4, angularCycles=8, interpolate=False,
-                           autoLog=False,ori=180,pos=(0,0))#this stim changes too much for autologging to be useful
+wedge3 = visual.RadialStim(myWin, tex='sqrXsqr', color=1, size=stimSize,
+                           visibleWedge=[0, 181], radialCycles=4, angularCycles=8, interpolate=False,
+                           autoLog=False, ori=180, pos=(0, 0))  # this stim changes too much for autologging to be useful
+wedge4 = visual.RadialStim(myWin, tex='sqrXsqr', color=-1, size=stimSize,
+                           visibleWedge=[0, 181], radialCycles=4, angularCycles=8, interpolate=False,
+                           autoLog=False, ori=180, pos=(0, 0))  # this stim changes too much for autologging to be useful
 
-initialOri=0
+initialOri = 0
 
-kwait = 1
-while kwait:
-    fixation.draw()
-    myWin.flip()
-    for key in event.getKeys():
-        if key in ['5']:
-            kwait = 0
-        elif key in ['escape','q']:
-            print(myWin.fps())
-            myWin.close()
-            core.quit()
 
-clock=core.Clock()
+compatibility.waitForScanner(myWin, fixation)
+
+
+clock = core.Clock()
 
 color_key = 'white'
-fn = 0;
+fn = 0
 trialClock = core.Clock()
 
 t = lastFPSupdate = 0
 t_p = 0
 
-nTargs = 0;
-nTargsH = 0;
-nTargsC = 0;
-nTargsF = 0;
-targTime= 1000;
-targFlag=0;
+# dict that keeps info related to hits, etc on fixation targets
+# should go into a funciont
+fixationInfo = {
+    'nTargsH': 0,
+    'nTargs': 0,
+    'nTargsC': 0,
+    'nTargsF': 0,
+    'targTime': 1000,
+    'targFlag': 0,
+    'color_key': 'white',
+    'fn': 0,
+    'my_colors': {'red': [1, 0, 0],
+                  'green': [0, 1, 0],
+                  'blue': [0, 0, 1],
+                  'yellow': [1, 1, 0]},
+    'fixLength': 1.0/2
+}
 
-while trialClock.getTime()<nullPeriod:#for 5 secs
-    t=trialClock.getTime()
-    t_diff=t-t_p
-    if t_diff > fixLength:
-        old_color_key = color_key
-        fnPrev = fn
-        while color_key == old_color_key: 
-            fn = np.random.randint(len(my_colors.keys()))
-            color_key = list(my_colors.keys())[fn]
-        this_color = my_colors[color_key]
-        fixation.setColor(this_color)
-        if fn>2:
-            nTargs = nTargs + 1
-            targTime = trialClock.getTime()
-            targFlag = 1
-        t_p = t
-        
-    fixation.draw()
+fixationInfo = compatibility.showNullPeriod(
+    myWin, fixation, fixationInfo, nullPeriod)
+# show the fixation cross for the null period
 
-    myWin.flip()
-    
-    for key in event.getKeys():
-        keyTime=trialClock.getTime()
-        if key in ['escape','q']:
-            print(myWin.fps())
-            myWin.close()
-            core.quit()
-        elif key in ['1','2','3','4']:
-            if targFlag:
-                if (keyTime-targTime)<1:
-                    nTargsC=nTargsC+1
-                    nTargsH=nTargsH+1
-                    targFlag=0
-            else:
-                nTargsC=nTargsC-1
-                nTargsF=nTargsF+1
+nTargs, nTargsH, nTargsC, nTargsF = 0, 0, 0, 0
+
+targFlag = 0
+targTime = 1000
+
+# timing info for this loop?
+# maybe should pass around global timer?
 
 t = lastFPSupdate = 0
 t_p = 0
 
-for i in range(0,(numBlocks)):
+for i in range(0, (numBlocks)):
     trialClock.reset()
-    t_p= 0
-    while trialClock.getTime()<blockLength:#for 5 secs
-        t=trialClock.getTime()
-        t_diff=t-t_p
+    t_p = 0
+    while trialClock.getTime() < blockLength:  # for 5 secs
+        t = trialClock.getTime()
+        t_diff = t-t_p
         if t_diff > fixLength:
             old_color_key = color_key
             fnPrev = fn
-            while color_key == old_color_key: 
+            while color_key == old_color_key:
                 fn = np.random.randint(len(my_colors.keys()))
                 color_key = list(my_colors.keys())[fn]
             this_color = my_colors[color_key]
             fixation.setColor(this_color)
-            if fn>2:
+            if fn > 2:
                 nTargs = nTargs + 1
                 targTime = trialClock.getTime()
                 targFlag = 1
             t_p = t
 
-        if trialClock.getTime()<blockLength/2:
-            if (t%flashPeriod) < (flashPeriod/2.0):# (NB more accurate to use number of frames)
+        if trialClock.getTime() < blockLength/2:
+            if (t % flashPeriod) < (flashPeriod/2.0):  # (NB more accurate to use number of frames)
                 stim = wedge1
             else:
                 stim = wedge2
         else:
-            if (t%flashPeriod) < (flashPeriod/2.0):# (NB more accurate to use number of frames)
+            if (t % flashPeriod) < (flashPeriod/2.0):  # (NB more accurate to use number of frames)
                 stim = wedge3
             else:
                 stim = wedge4
@@ -234,24 +217,24 @@ for i in range(0,(numBlocks)):
         fixation.draw()
 
         myWin.flip()
-    
+
         for key in event.getKeys():
-            keyTime=trialClock.getTime()
-            if key in ['escape','q']:
+            keyTime = trialClock.getTime()
+            if key in ['escape', 'q']:
                 print(myWin.fps())
                 myWin.close()
                 core.quit()
-            elif key in ['1','2','3','4']:
+            elif key in ['1', '2', '3', '4']:
                 if targFlag:
-                    if (keyTime-targTime)<1:
-                        nTargsC=nTargsC+1
-                        nTargsH=nTargsH+1
-                        targFlag=0
+                    if (keyTime-targTime) < 1:
+                        nTargsC = nTargsC+1
+                        nTargsH = nTargsH+1
+                        targFlag = 0
                 else:
-                    nTargsC=nTargsC-1
-                    nTargsF=nTargsF+1
-    
-nTargsC=max(nTargsC,0)
+                    nTargsC = nTargsC-1
+                    nTargsF = nTargsF+1
+
+nTargsC = max(nTargsC, 0)
 print("nTargs:", int(nTargs))
 print("nTargsH:", int(nTargsH))
 print("nTargsF:", int(nTargsF))
@@ -259,6 +242,7 @@ print("nTargsC:", int(nTargsC))
 
 print("Score: %.2f" % (nTargsC/nTargs*100))
 
+compatibility.endExperiment(myWin)
+
 myWin.close()
 core.quit()
-
